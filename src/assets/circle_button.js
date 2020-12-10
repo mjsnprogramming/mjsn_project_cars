e6bab29b878153f8714f334d82572e74738ae37f
Majson <majsonlbn@gmail.com>
2020-12-10 22:10:19 +0100

    nothing

--- src/assets/circle_button.js
+++
@@ -1,9 +0,0 @@
-const circle = document.querySelector('#circle');
-
-const circle_on_click = () => {
-  circle.addEventListener('click', (circle_button) => {
-    circle_button.style.transform = "translate(0, 0)";
-    console.log('clicking');
-  })
-};
-circle_on_click();
--- src/assets/header_dropdown.js
+++ src/assets/header_dropdown.js
@@ -1,19 +1,16 @@
-const brands = document.querySelector('#brands');
+const brands = document.querySelector("#brands");
 const countries = document.querySelector('#countries');
-const years = document.querySelector('#years');
+const years = document.querySelector("#years");
 const drop_brands = document.querySelector('#drop_brands');
 const drop_countries = document.querySelector('#drop_countries');
-const drop_years = document.querySelector('#drop_years');
+const drop_years = document.querySelector("#drop_years");
+const nav = document.querySelector('#nav');
 
-brands.addEventListener('mouseover', () => {
-    drop_brands.style.visibility = 'visible';
+years.addEventListener("mouseover", () => {
+    drop_years.style.visibility = "visible";
   }
 );
-countries.addEventListener('mouseover', () => {
-    drop_countries.style.visibility = 'visible';
-  }
-);
-years.addEventListener('mouseover', () => {
-    drop_years.style.visibility = 'visible';
-  }
-);
+years.addEventListener("mouseout", () => {
+    drop_years.style.visibility = "hidden";
+});
+
