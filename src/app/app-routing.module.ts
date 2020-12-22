4f166c5cbab584927c838b981e834bbb2dd5bb59
Majson <majsonlbn@gmail.com>
2020-12-22 22:02:37 +0100

    routing for sub pages demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SubCarPagesComponent } from './sub-car-pages/sub-car-pages.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sub-car-pages', component: SubCarPagesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
