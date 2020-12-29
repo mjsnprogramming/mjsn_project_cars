109073d54e7bd0ee2208b946cd270534423c3d3b
Majson <majsonlbn@gmail.com>
2020-12-29 14:48:39 +0100

    searcher

--- src/assets/header_dropdown.js
+++ src/assets/header_dropdown.js
@@ -7,6 +7,8 @@ const drop_countries = document.querySelector('#drop_countries');
 const drop_years = document.querySelector('#drop_years');
 const nav = document.querySelector('#nav');
 const drop_nav = document.querySelector('#drop_nav');
+const search = document.querySelector('fa-icon');
+const drop_search = document.querySelector('#drop_search');
   brands.addEventListener("mouseover", () => {
     drop_brands.style.visibility = "visible";
   });
@@ -32,7 +34,23 @@ const drop_nav = document.querySelector('#drop_nav');
     else {
       drop_nav.style.display = "";
     }
-  })
+  });
+  search.addEventListener('click', () => {
+    if (drop_search.style.display === "") {
+      drop_search.style.display = "block";
+    }
+    else {
+      drop_search.style.display = "";
+    }
+    if (drop_search.style.display === "block") {
+      drop_brands.style.visibility = "hidden";
+      drop_countries.style.visibility = "hidden";
+      drop_years.style.visibility = "hidden";
+      drop_brands.style.cursor = "default";
+    }
+  });
+
+
 };
 
 
